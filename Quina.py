82bcc26acb291f8ad5372bfe8651a5b3698b4701
Deandreson Alves <deandreson@alumni.usp.br>
2024-10-07 16:04:42 -0300

    Salvando as alteraçõe que fiz para exibir os numeros da QUina

--- Quina.py
+++ Quina.py
@@ -1,1 +1,27 @@
-print("teste")
+#### Execução ###
+#   python3 -m venv venv
+#	source venv/bin/activate
+#	cd Aplicativos/Inquiri
+#   export FLASK_ENV=development 
+#   export FLASK_APP=Quina
+###########################################
+from flask import Flask, render_template, request, url_for, redirect,jsonify
+
+import pandas as pd
+
+# --------------------------------------------------------#
+df = pd.read_excel('data/Quina.xlsx')
+df = df.head(5)
+# Conver# Converter o DataFrame para JSON
+df_json = df.to_json(orient='records', force_ascii=False)
+
+
+
+# ----------------------------------------------------------#
+app = Flask(__name__)
+@app.route('/') 
+def index():
+    return render_template('index.html',jogos=df_json)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ data/schema_quina.sql
@@ -0,0 +1,13 @@
+Concurso	
+Data Sorteio	
+Bola1	Bola2	Bola3	Bola4	Bola5	
+Ganhadores 5 acertos	
+Cidade / UF	
+Rateio 5 acertos	
+Ganhadores 4 acertos	Rateio 4 acertos	
+Ganhadores 3 acertos	Rateio 3 acertos	
+Ganhadores 2 acertos	Rateio 2 acertos	
+Acumulado 5 acertos	Arrecadacao Total	
+Estimativa Premio	
+Acumulado Sorteio Especial Quina de São João
+observação
